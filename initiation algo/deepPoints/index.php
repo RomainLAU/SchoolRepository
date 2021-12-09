6023210e4bbff616f6b47c5d775fb932a014b259
RomainLau <romain.laurent@epitech.digital>
2021-12-09 20:33:56 +0100

    Devoir non terminé

--- initiation algo/deepPoints/index.php
+++ initiation algo/deepPoints/index.php
@@ -16,7 +16,7 @@ foreach ($inputs as $line) {
 
 function deepestValues ($map) {
     $directions = [
-        ['y' => 1, 'x' => -1],
+        ['y' => 0, 'x' => -1],
         ['y' => -1, 'x' => 0],
         ['y' => 0, 'x' => 1],
         ['y' => 1, 'x' => 0],
