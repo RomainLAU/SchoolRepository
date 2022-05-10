f609b40880547555ec6ad95d06b01133fb393c4a
RomainLAU <romain.laurent@epitech.digital>
2022-05-10 17:45:48 +0200

    added the post route

--- Emir/NodeJS/app.js
+++ Emir/NodeJS/app.js
@@ -1,18 +1,21 @@
-const express = require('express');
-const fs = require('fs');
-const mysql = require('mysql2');
-const { createServer } = require("http");
-const { Server } = require("socket.io");
+const express = require('express')
+const fs = require('fs')
+const mysql = require('mysql2')
+const { createServer } = require("http")
+const { Server } = require("socket.io")
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
 
-const httpServer = createServer(app);
-const io = new Server(httpServer, { });
+const httpServer = createServer(app)
+const io = new Server(httpServer, { })
 
 io.on("connection", (socket) => {
-    // console.log("Connection")
-});
+    console.log("New connection")
+})
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.set('view engine', 'ejs')
 
@@ -21,7 +24,7 @@ const connection = mysql.createConnection({
     user: 'root',
     password: 'tiger',
     database: 'cinema'
-});
+})
 
 app.get('/films', (req, res) => {
 
@@ -37,7 +40,6 @@ app.get('/films/:id', (req, res) => {
     let id = req.params.id
 
     connection.query(`SELECT * FROM films WHERE id_film = ?`, [id], (err, results, fields) => {
-        console.log(results)
         res.render('film', {
             film: results[0]
         })
@@ -56,5 +58,17 @@ app.delete('/api/films/:id', (req, res) => {
     })
 
 })
- 
+
+app.post('/api/films/:titre', (req, res) => {
+    let titre = req.params.titre
+    connection.query(`INSERT INTO films (id_genre, id_distributeur, titre, resum, date_debut_affiche, date_fin_affiche, duree_minutes, annee_production) VALUES (1, 1, ?, 'aaaaaaaaaaaaaa', '2003-07-20', '2300-08-27', 129, 2003)`, 
+    [titre],
+    (err, results, fields) => {
+        console.log(err)
+        res.json({status: 201, data: "Created with success"})
+
+        io.emit("film-create", titre)
+    })
+})
+
 httpServer.listen(port)
