bd0af73a68b6172608379acf5cbd9fec5e8dbf74
RomainLAU <romain.laurent@epitech.digital>
2022-10-17 12:56:01 +0200

    Fix flattenList and frequencyOfLetters exercises

--- 2021-2022/hackinscience/OnlyDoneHere/flattenList.py
+++ 2021-2022/hackinscience/OnlyDoneHere/flattenList.py
@@ -1,11 +1,28 @@
-import functools
-import operator
+# from functools import reduce
+# from operator import iconcat
 
-def flatten(a_list):                
+# def flatten(a_list):                
+#     # flatten_list = [reduce(iconcat, a_list, [])]
+#     # if any(isinstance(item, list) for item in a_list):
+#     #     print(flatten_list)
+#     #     flatten(flatten_list)
+#     # return flatten_list
 
-    print(functools.reduce(operator.iconcat, a_list, []))
-
-    # print(flatten_list)
+#     return [reduce(iconcat, a_list, [])]
 
 
-flatten([[1], [[3, 4], 5], [[[]]], [[[6]]], []])
+# print(flatten([[1], [[3, 4], 5], [[[]]], [[[6]]], []]))
+
+from collections import abc
+
+def yield_flatten(a_list):
+    for item in a_list:
+        if isinstance(item, abc.Iterable) and not isinstance(item, (str, bytes)):
+            yield from yield_flatten(item)
+        else:
+            yield item
+
+def flatten(a_list):
+    return(list(yield_flatten(a_list)))
+
+print(flatten([[1], [[3, 4], 5], [[[]]], [[[6]]], []]))
--- 2021-2022/hackinscience/OnlyDoneHere/frequencyOfLetters.py
+++ 2021-2022/hackinscience/OnlyDoneHere/frequencyOfLetters.py
@@ -7,7 +7,7 @@ with open('words.txt', 'rt') as f:
     for letter in ascii_lowercase:
         if letter in file:
             nbOfLetter = file.count(letter)
-            frequences[letter] = round(nbOfLetter / textLength, 2)
+            frequences[letter] = "{:.2f}".format(nbOfLetter / textLength)
 
 for key in frequences:
     print(f"{key}: {frequences[key]}", sep="")
