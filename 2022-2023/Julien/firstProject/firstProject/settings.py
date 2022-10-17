95bcc52a03e207e42bbe6b3f1e08b6ca821537de
RomainLAU <romain.laurent@epitech.digital>
2022-10-17 17:04:39 +0200

    Add html template for django

--- 2022-2023/Julien/firstProject/firstProject/settings.py
+++ 2022-2023/Julien/firstProject/firstProject/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "debug_toolbar",
 ]
 
 MIDDLEWARE = [
@@ -48,9 +49,14 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "debug_toolbar.middleware.DebugToolbarMiddleware",
 ]
 
 ROOT_URLCONF = "firstProject.urls"
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
 
 TEMPLATES = [
     {
--- 2022-2023/Julien/firstProject/hello/views.py
+++ 2022-2023/Julien/firstProject/hello/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+from hello.models import Article
+
+
+def index(request):
+
+    articles = Article.objects.all()
+
+    return render(
+        request, "index.html", {"param": "complètement vrai", "articles": articles}
+    )
