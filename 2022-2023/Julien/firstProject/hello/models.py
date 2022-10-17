7c24af34fa99d8a35045e3c3e1af0e1878e6d6d5
RomainLAU <romain.laurent@epitech.digital>
2022-10-17 15:48:48 +0200

    Ignore unwanted files and directories

--- 2022-2023/Julien/firstProject/hello/models.py
+++ 2022-2023/Julien/firstProject/hello/models.py
@@ -1,3 +1,7 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
 
-# Create your models here.
+
+class Article(models.Model):
+    title = models.CharField(max_length=512)
+    body = models.TextField(blank=True, default="")
