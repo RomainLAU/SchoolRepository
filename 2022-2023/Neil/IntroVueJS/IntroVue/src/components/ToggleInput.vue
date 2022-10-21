ee65eda680e887f2732266eed8f2db7e7d7c0d80
RomainLAU <romain.laurent@epitech.digital>
2022-10-21 13:10:26 +0200

    Create a toggle button in vue project

---
+++ 2022-2023/Neil/IntroVueJS/IntroVue/src/components/ToggleInput.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+const props = defineProps<{
+  modelValue: boolean;
+}>();
+
+const emit = defineEmits(["update:modelValue"]);
+
+function onToggle() {
+  emit("update:modelValue", !props.modelValue);
+}
+</script>
+
+<template>
+  <label class="w-10 h-5 border border-black flex">
+    <div
+      class="w-0 transition-all duration-100 ease-in-ease-out"
+      :style="[modelValue ? 'flex-grow: 1' : 'flex-grow: 0']"
+    ></div>
+    <div class="bg-black w-5 h-5 rounded-full"></div>
+    <input type="checkbox" :value="modelValue" @input="onToggle" hidden
+  /></label>
+</template>
--- 2022-2023/Neil/IntroVueJS/IntroVue/src/views/HomeView.vue
+++ 2022-2023/Neil/IntroVueJS/IntroVue/src/views/HomeView.vue
@@ -1,51 +1,12 @@
+<!-- eslint-disable @typescript-eslint/no-unused-vars -->
 <script setup lang="ts">
-import { computed, ref, watch } from "vue";
+import ToggleInput from "../components/ToggleInput.vue";
+import { ref } from "vue";
 
-const count = ref(0);
-
-function decrease() {
-  count.value -= 1;
-}
-function increase() {
-  count.value++;
-}
-
-const double = computed(() => {
-  return count.value * 2;
-});
-
-const students = ref([
-  { id: 1, name: "Luis" },
-  { id: 2, name: "Romain" },
-  { id: 3, name: "Mika" },
-  { id: 4, name: "William" },
-]);
+const isActive = ref(false);
 </script>
 
 <template>
-  <main class="p-16 bg-[#E19ED]">
-    <button
-      @click="increase"
-      class="px-4 h-8 bg-slate-200 border border-gray-300"
-    >
-      Increase
-    </button>
-    <button
-      @click="decrease"
-      class="px-4 h-8 bg-slate-200 border border-gray-300"
-    >
-      Decrease
-    </button>
-    <h2>Count: {{ count }}</h2>
-    <p :class="[count % 2 === 0 ? 'bg-green-500' : 'bg-red-500']">
-      {{ count % 2 === 0 ? "Count is even" : "Count is odd" }}
-    </p>
-    <h2>Doubled value: {{ double }}</h2>
-
-    <ul>
-      <li v-for="student in students" :key="student.id">
-        {{ student.name }}
-      </li>
-    </ul>
-  </main>
+  <ToggleInput v-model="isActive" />
+  <!-- <p>{{ isActive }}</p> -->
 </template>
